53e761da2bb43dc4f4db3467c6604e241d7ea254
Sergey-Markov <1nativefl@gmail.com>
2021-07-26 19:18:43 +0300

    add slider, add Swiper library, add two images for example

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
+import 'swiper/swiper-bundle.css';
 import './sass/main.scss';
 import './js/ApiService';
+
+import Swiper from 'swiper';
+import SwiperCore, { EffectCoverflow, Navigation, Pagination } from 'swiper/core';
+  // configure Swiper to use modules
+  SwiperCore.use([EffectCoverflow, Navigation, Pagination]);
+
+
+const swiper = new Swiper('.swiper-container', {
+
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        hideOnClick: true,
+      },
+    
+  });
