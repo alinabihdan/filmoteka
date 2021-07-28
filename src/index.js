b96821fd17ce5d6a0318912959d1406102874089
Sergey-Markov <1nativefl@gmail.com>
2021-07-28 14:47:34 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import './js/modal-ourteam';
 import './sass/main.scss';
 import './js/ApiService';
-<<<<<<< HEAD
-
-
-
-
-  
-  
-  
-
-  
-=======
 import './js/slider';
 import './js/modal-film';
 import './js/library';
 import './js/pagination';
 import './js/btn-to-close';
 import './js/btn-to-top';
->>>>>>> 042f1341a829e79e4d4084edbfd7ab78db4179db
