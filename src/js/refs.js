0b72f90788c6e1597cb3afad07355c772f7dd3ed
alinabihdan <alina.bigdan1@gmail.com>
2021-07-28 16:38:45 +0300

    по запросу на апи делается разметка кнопок

--- src/js/refs.js
+++ src/js/refs.js
@@ -20,4 +20,5 @@ export default {
   modalFilmBlackdrop: document.querySelector('.film-blackdrop'),
   bodyEl: document.querySelector('body'),
   buttonToTop: document.getElementById('btn-to-top'),
+  genresContainer: document.getElementById('genre-container'),
 };
