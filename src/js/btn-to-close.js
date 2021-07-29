01f6264be03ec09b9df49d8595622dd552c13aed
alinabihdan <alina.bigdan1@gmail.com>
2021-07-30 00:42:23 +0300

    баг рефов

--- src/js/btn-to-close.js
+++ src/js/btn-to-close.js
@@ -1,9 +1,9 @@
-import refsB from './refs';
+import refs from './refs';
 
 // const refs = {
 //   closeBtn: document.querySelector('[data-action = "close"]'),
 //   modalWindow: document.querySelector('.film-blackdrop'),//Тут должен быть класс или айди модалки
-//   // modalOverlay: document.querySelector(''), //Тут должен быть класс или айди оверлея модалки 
+//   // modalOverlay: document.querySelector(''), //Тут должен быть класс или айди оверлея модалки
 // }
 
 refs.closeBtn.addEventListener('click', closeModal);
@@ -12,21 +12,21 @@ refs.modalWindow.addEventListener('click', overlayClick);
 window.addEventListener('keydown', keyPress);
 
 function keyPress(event) {
-  //escape key 27 
+  //escape key 27
   const pressedKey = event.keyCode;
-  const modalIsOpen = refs.modalWindow.classList.contains('is-active');//Тут должен быть класс/айди открытой модалки i.e "is-open", "modal-open"
-  if(pressedKey === 27 && modalIsOpen) {
+  const modalIsOpen = refs.modalWindow.classList.contains('is-active'); //Тут должен быть класс/айди открытой модалки i.e "is-open", "modal-open"
+  if (pressedKey === 27 && modalIsOpen) {
     closeModal();
   }
 }
 
-function overlayClick (e) {
+function overlayClick(e) {
   if (e.target.classList.contains('film-blackdrop')) {
     closeModal();
-  };
+  }
 }
 
 function closeModal() {
-  refs.modalWindow.classList.remove('is-active'); //Тут должен быть класс/айди который мы убираем с модалки для того чтобы ее спрятать 
+  refs.modalWindow.classList.remove('is-active'); //Тут должен быть класс/айди который мы убираем с модалки для того чтобы ее спрятать
   refsB.bodyEl.classList.remove('modal-open');
 }
