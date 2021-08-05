d4ca3b67ce6ba3d2cf763afe9414b485b56a4e9d
Petro Babiuk <75635325+PetroBabiuk@users.noreply.github.com>
2021-08-05 14:20:59 +0300

    Revert "добавляет файл для описания логики показа трейлеров"
    This reverts commit 336c26f640308adc0ef8d66d4642b235c41b1e5e.

--- src/js/trailers.js
+++
@@ -1,8 +0,0 @@
-import * as basicLightbox from 'basiclightbox';
-import 'basiclightbox/dist/basicLightbox.min.css';
-import refs from './refs';
-import filmoteka from './ApiService';
-
-function fetchTrailerFilm(id) {
-    return filmoteka.getMovieByID
-}
